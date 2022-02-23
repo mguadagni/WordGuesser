0dba2fd89e27e8135aef2f4842adcd157f61102b
mguadagni <guadagnimichael@gmail.com>
2022-02-22 19:02:04 -0500

    Starting with the very first steps.

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,5 +1,12 @@
 public class GuessingGame {
     public static void main(String[] args) {
-        System.out.println("Initial Commit");
+        System.out.println("Welcome to Word Guesser!");
+        guessingGame();
+    }
+
+    private static void guessingGame() {
+        String playableWords = "Michael";
+        String guessedCharacter = UI.readString("Please enter your guess.");
+        System.out.println(guessedCharacter);
     }
 }
---
+++ src/UI.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class UI {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static String readString (String question) {
+        System.out.println(question + "\nInput: ");
+        String inputString = scanner.nextLine();
+        return inputString;
+    }
+}
