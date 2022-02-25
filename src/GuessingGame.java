7ce86d7789d93cdcfecf858d4a0a703412af3666
mguadagni <guadagnimichael@gmail.com>
2022-02-24 19:33:07 -0500

    implemented finding the index of the guessed character

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class GuessingGame {
+
+    private static ArrayList<Character> charStorage = new ArrayList<Character>(10);
+
     public static void main(String[] args) {
         System.out.println("Welcome to Word Guesser!");
         guessingGame();
@@ -9,19 +15,39 @@ public class GuessingGame {
         char guessedCharacter = UI.readString("Please enter your guess.").charAt(0);
         //System.out.println(guessedCharacter);
 
-        int numOfChar = 0;
+//        int numOfChar = 0;
+//
+//        for (int i = 0; i < playableWords.length(); i++) {
+//            if (guessedCharacter == playableWords.charAt(i)) {
+//                numOfChar++;
+//                charStorage.add(playableWords.charAt(i));
+//            }
+//        }
 
-        for (int i = 0; i < playableWords.length(); i++) {
-            if (guessedCharacter == playableWords.charAt(i)) {
+        int numOfChar = 0;
+        for(int i = 0; i < playableWords.length(); i++){
+            if(guessedCharacter == playableWords.charAt(i)){
                 numOfChar++;
             }
         }
+        int[] charArr = new int[numOfChar];
+        numOfChar = 0;
+        for(int i = 0; i < playableWords.length(); i++){
+            if(guessedCharacter == playableWords.charAt(i)){
+                charArr[numOfChar] = i;
+                numOfChar++;
+            }
+        }
+
+        //System.out.println(charStorage);
 
         if (numOfChar > 0) {
             System.out.println(guessedCharacter + " is correct and was found " + numOfChar + " times.");
         } else {
             System.out.println(guessedCharacter + " was not found, try again.");
         }
+
+        System.out.println(Arrays.toString(charArr));
 
         //System.out.println(playableWords.indexOf(guessedCharacter));
         //System.out.println(playableWords.charAt(playableWords.indexOf(guessedCharacter)));
